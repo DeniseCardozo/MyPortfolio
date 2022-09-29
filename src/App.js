01fefd26e4a890cfe2d0b8697baf8b458ac52c88
DeniseCardozo <dennicardozo96@gmail.com>
2022-09-29 11:26:37 -0300

    Change render to principal component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Principal from './Components/Principal/Principal';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Este será mi porfolio! 
-        </p>
-      </header>
-    </div>
+    <Principal />
   );
 }
 
